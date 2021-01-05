87ca637060f8e889cea1f550e2156cafdb052fb9
momchilmitev <momchilmitev87@gmail.com>
2021-01-05 15:27:55 +0100

    added simple css reset and the basic skeleton of all components needed to structure the app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,31 @@
 import "./App.scss";
+import ToggleButton from "../ToggleButton/ToggleButton";
+import TotalCard from "../TotalCard/TotalCard";
+import OverviewCard from "../OverviewCard/OverviewCard";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="header">
+          <div className="title">
+            <h1 className="title__text">Social Media Dashboard</h1>
+            <p className="title__description">Total Followers: 23,004</p>
+          </div>
+          <ToggleButton />
+        </header>
+        <main className="main">
+          <section className="total">
+            <TotalCard />
+          </section>
+          <section className="overview">
+            <h2 className="overview__title">Overview - Today</h2>
+            <OverviewCard />
+          </section>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
