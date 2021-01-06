9523b2c5e345a6e4fa1263f289f543d6d691afa1
momchilmitev <momchilmitev87@gmail.com>
2021-01-06 16:17:39 +0100

    added static data, made the total card component customizable and rendered the content for the total section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,56 @@ import ToggleButton from "../ToggleButton/ToggleButton";
 import TotalCard from "../TotalCard/TotalCard";
 import OverviewCard from "../OverviewCard/OverviewCard";
 
+const socials = [
+  {
+    icon: "facebook",
+    user: "@nathanf",
+    total: 1987,
+    today: [12, "up"],
+    views: 87,
+    viewsPercentage: [3, "up"],
+    likes: 52,
+    likesPercentage: [2, "down"],
+  },
+  {
+    icon: "twitter",
+    user: "@nathanf",
+    total: 1044,
+    today: [88, "up"],
+    views: 117,
+    viewsPercentage: [303, "up"],
+    likes: 507,
+    likesPercentage: [553, "up"],
+  },
+  {
+    icon: "instagram",
+    user: "@realnathanf",
+    total: "11K",
+    today: [1099, "up"],
+    views: "52K",
+    viewsPercentage: [1375, "up"],
+    likes: 5462,
+    likesPercentage: [2257, "up"],
+  },
+  {
+    icon: "youtube",
+    user: "Nathan F.",
+    total: 8239,
+    today: [144, "down"],
+    views: 1407,
+    viewsPercentage: [12, "down"],
+    likes: 107,
+    likesPercentage: [19, "down"],
+  },
+];
+
 function App() {
+  const renderTotalContent = () => {
+    return socials.map((media) => (
+      <TotalCard key={media.total} media={media} />
+    ));
+  };
+
   return (
     <div className="app app--dark">
       <div className="container">
@@ -15,9 +64,7 @@ function App() {
           <ToggleButton />
         </header>
         <main className="main">
-          <section className="total">
-            <TotalCard />
-          </section>
+          <section className="total">{renderTotalContent()}</section>
           <section className="overview">
             <h2 className="overview__title">Overview - Today</h2>
             <OverviewCard />
--- src/components/TotalCard/TotalCard.js
+++ src/components/TotalCard/TotalCard.js
@@ -1,21 +1,36 @@
 import "./TotalCard.scss";
 import facebook from "../../assets/icons/icon-facebook.svg";
+import instagram from "../../assets/icons/icon-instagram.svg";
+import twitter from "../../assets/icons/icon-twitter.svg";
+import youtube from "../../assets/icons/icon-youtube.svg";
 import upArrow from "../../assets/icons/icon-up.svg";
+import downArrow from "../../assets/icons/icon-down.svg";
 
-const TotalCard = () => {
+const icons = {
+  facebook: facebook,
+  twitter: twitter,
+  instagram: instagram,
+  youtube: youtube,
+  up: upArrow,
+  down: downArrow,
+};
+
+const TotalCard = ({ media }) => {
   return (
     <article className="total-card total-card--dark">
       <div className="total-card__user">
-        <img src={facebook} alt="social media icon" />
-        <span>@nathanf</span>
+        <img src={icons[media.icon]} alt="social media icon" />
+        <span>{media.user}</span>
       </div>
       <div className="total-card__total-followers">
-        <p className="total-card__total-followers--count">1987</p>
-        <p className="total-card__total-followers--text">followers</p>
+        <p className="total-card__total-followers--count">{media.total}</p>
+        <p className="total-card__total-followers--text">
+          {media.icon === "youtube" ? "subscribers" : "followers"}
+        </p>
       </div>
       <div className="total-card__todays-followers">
-        <img src={upArrow} alt="arrow icon" />
-        <span>12 Today</span>
+        <img src={icons[media.today[1]]} alt="arrow icon" />
+        <span>{media.today[0]} Today</span>
       </div>
     </article>
   );
